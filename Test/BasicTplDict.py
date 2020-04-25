5149a31e3af2d04b96e46ddbfab5dea7bcb52946
Maurizio Spadari <maurizios63@gmail.com>
2020-04-25 15:59:30 +0200

    Initial commit:
     -- Flat structure working

--- Test/BasicTplDict.py
+++ Test/BasicTplDict.py
@@ -4,23 +4,36 @@ my_init_dict = {
 
     'a': 123,
     'b': 123.45,
-    'c': 'John Doe'
+    'c': 'John Doe',
+    'd': {'a': 1.5, 'b': 4},
+     '__default__' : 10
 }
 
 user_dict = {
 
     'a': 143,
-    'b': 445.33,
-    'c': 'Mike Smith'
+    'b': 445.5,
+    'c': 'Mike Smith',
+    'd': {'a': 3, 'b': 7}
 }
+
+
 logging.basicConfig(level=logging.INFO)
 
 my_dict_obj = TplDictC(my_init_dict)
+logging.info(my_dict_obj)
+logging.info('Keys : %s', '.'.join(my_dict_obj.keys()))
+logging.info("----------")
+my_dict_obj.update(user_dict, d={'x':5}, f=7.1)
+logging.info(my_dict_obj)
+logging.info("----------")
+my_dict_obj.update(b=65.13)
+logging.info(my_dict_obj)
+logging.info("----------")
+my_dict_obj['a']=20
+logging.info(my_dict_obj)
+logging.info("----------")
+my_dict_obj['a']=20.5
+logging.info(my_dict_obj)
 
-my_dict_obj.update(user_dict)
 
-for (key, value) in my_dict_obj.items():
-    print(key,value)
-
-xx = my_init_dict
-xx.update(user_dict, m)
--- TplPkg/TplDict.py
+++ TplPkg/TplDict.py
@@ -3,33 +3,124 @@ import logging
 
 class TplDictC (dict):
 
-    def __init__(self,init_dict={}):
+    # Name of default key
+    __default_key__ = '__default__'
+    __logging_level = logging.INFO
+
+    def __init__(self, *args, **kwargs):
         """
         Init method
         set type for each entry
         :param init_dict: Initialization dictionary to define types
         """
-        super(TplDictC, self).__init__()
         self.logger = logging.getLogger('TplDictC')
-        # Now process each keyboard entry for init. dictionary
-        self.type_d = {key: type(value) for (key, value) in init_dict.items()}
-        self.logger.info("Init. done")
+        self.logger.setLevel(TplDictC.__logging_level)
+        super(TplDictC, self).__init__(*args, **kwargs)
+        self.hidden_key_l = [TplDictC.__default_key__]
+        # Initialize the set of keys that have default values
+        self.default_key_set = self.get_keys
+        self.has_default_type = self.get(TplDictC.__default_key__) is not None
+        self.logger.debug("Init. done")
 
-    def update(self, user_dict, **kwargs):
+    def __setitem__(self, key, value):
         """
-        Put the real value from the user dictionary (checking the types)
-        :param user_dict:
+        Over-ride of set method:
+            my_dict[key] = value
+        Checks entries validity & then placed value into dictionary
+        :param key:
+        :param value:
         :return:
         """
-        for (key, value) in user_dict.items():
+        if self.check_tuple(key, value):
+            super(TplDictC, self).__setitem__(key, value)
+            try:
+                # Removed object from list
+                self.default_key_set.remove(key)
+            except ValueError:
+                # If element is not present it means that has laready been update
+                self.logger.warn("Value of key %s is super-seeded" % key)
+
+    def __iter__(self):
+        return iter(self.get_keys)
+
+    def keys(self):
+        """
+        Over-ride default dict values method
+        :return: all keys except the 'hidden' key : __default
+        """
+        return self.get_keys
+
+    def values(self):
+        """
+        Over-ride default dict values method
+        :return: all key-values except the ones of 'hidden' keys : __default
+        """
+        return [self[key] for key in self.get_keys]
+
+    def itervalues(self):
+        """
+        Over-ride default dict values method
+        :return: all key-values except the ones of 'hidden' keys : __default
+        """
+        return (self[key] for key in self.get_keys)
+
+    def update(self, *args, **kwargs):
+        """
+        Over-rides update method with a check of tuples
+        :return:
+        """
+        # Process args
+        new_dict = {}  # Initialize new_dict
+        len_args = len(args)
+        if len_args > 1:
+            # As in default update method only 1 positional argument is allowed
+            raise TypeError('update expected at most 1 arguments, got %d' % len_args)
+        elif len_args == 1:
+            # A dictionary has been specified as positional argument
+            new_dict = args[0]
+        new_dict.update(kwargs)  # Update dictionary with positional arguments
+        for (key, value) in new_dict.items():
             # Check for invalid keys
-            if key not in self.type_d.keys():
-                self.logger.error("Invalid key %s" % key)
-            elif type(value) != self.type_d[key]:
-                self.logger.error("Invalid type %s for values %s (key %s): expected %s" %
-                                  (type(value), value, key, self.type_d[key]))
-            else:
-                super(TplDictC,self).__setitem__(key,value)
-        self.logger.info("Update done")
+            self.__setitem__(key,value)
+            # super(TplDictC, self).__setitem__(key, value)
+        self.logger.debug("Update done")
 
+    @property
+    def get_keys(self):
+        """
+        Procedure to return keys except the hidden ones
+        :return:
+        """
+        return [key for key in super(TplDictC, self).keys() if key not in self.hidden_key_l]
 
+    def check_tuple(self, key, value):
+        """
+        Verify if key, value tuple is valid
+        :param key:
+        :param value:
+        :return: True if valid
+        """
+        # Check in standard keys
+        if key in self.get_keys:
+            ref_value = self[key]
+        elif self.has_default_type:
+            # Otherwise use default type if defined
+            ref_value = self[TplDictC.__default_key__]
+        else:
+            # Error out
+            self.logger.error("Invalid key %s" % key)
+            return False
+        # Now check if type matches
+        if type(value) is not type(ref_value):
+            self.logger.error("Invalid type %s for key %s: expected %s" %
+                              (type(value), key, type(ref_value)))
+            return False
+        else:
+            return True
+
+    def __repr__(self):
+        """
+        Over-rise default representation omitting hidden keys
+        :return:
+        """
+        return { x: self[x] for x in self.get_keys}.__repr__()
